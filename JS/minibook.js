c244eb8b6aad7c67e53f4e50c71678c8e596ca1f
yoonhye-ju <93234942+yoonhye-ju@users.noreply.github.com>
2022-01-25 13:06:52 +0900

    revise
    revise

---
+++ JS/minibook.js
@@ -0,0 +1,19 @@
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=title",
+  data: { query: "2022 선재국어", sort: "latest", size: 10 },
+  headers: { Authorization: "KakaoAK c520cb5172aeec952fa88823508d155b" }
+})
+  .done(function (msg) {
+    console.log(msg);
+    var minibooks = document.getElementsByClassName('rightBox_quentity_left');
+
+    for (var i = 0; i < minibooks.length; i++) {
+      $(".rightBox_quentity_left").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
+
+      var str5 = msg.documents[i].title;
+      var str6 = str5.substring(0, 32);
+
+      $(".rightBox_quentity_left").eq(i).append("<h4>" + str6 + "</h4>");
+    }
+  });
