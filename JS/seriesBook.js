341034146c0dff8ce9adb5777803912ee198ec66
yoonhye-ju <93234942+yoonhye-ju@users.noreply.github.com>
2022-01-26 10:00:25 +0900

    revise
    revise

--- JS/seriesBook.js
+++ JS/seriesBook.js
@@ -1,21 +1,22 @@
 $.ajax({
   method: "GET",
   url: "https://dapi.kakao.com/v3/search/book?target=title",
-  data: { query: "2022 국어왕국" , sort: "latest", size: 10},
+  data: { query: "2022" , sort: "latest", size: 10},
   headers: { Authorization: "KakaoAK c520cb5172aeec952fa88823508d155b" }
 })
   .done(function (msg) {
-    var seriesBooks = document.getElementsByClassName('seriesBook');
+    console.log(msg);
+    console.log(msg.documents[0].thumbnail);
+    var seriesBooks = document.querySelectorAll('.seriesBook');
 
     for (var i = 0; i < seriesBooks.length; i++) {
-      $(".seriesBook").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
-      $(".seriesBook").eq(i).append("<span>" + msg.documents[i].publisher + "</span>");
+      $(".toic").eq(i).children(".seriesbook").append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
 
       var str7 = msg.documents[i].title;
       var str8 = str7.substring(0, 20);
 
-      $(".seriesBook").eq(i).append("<h4>" + str8 + "</h4>");
+      $(".toic").eq(i).children(".seriesbook").append("<h4>" + str8 + "</h4>");
 
-      $(".seriesBook").eq(i).append("<p>" + msg.documents[i].price + "</p>");
+      $(".toic").eq(i).children(".seriesbook").append("<p>" + msg.documents[i].price + "</p>");
     }
   });
